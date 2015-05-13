82d63fda9cc6198e48032d980cc6d3bbe6812ded
c3h3 <c3h3.tw@gmail.com>
2015-05-13 16:54:05 +0800

    update convert script

--- convert2RPTT.sh
+++ convert2RPTT.sh
@@ -1,2 +1,4 @@
 git filter-branch --prune-empty --subdirectory-filter CaseStudies/Case6PttGossiping/RPTT/ --tag-name-filter cat -- --all
-
+git remote remove origin
+git remote add github https://github.com/agilearning/RPTT.git
+git push -f origin master
